b359c74a8e71c717ea6535052eb5b92b4b79a966
lucifer-morningstar <slobeck@gmail.com>
2025-09-02 22:47:39 -0700

    added a bool for HASRUN

--- scripts/gbn.sh
+++ scripts/gbn.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 gbn_include=$CURRENT_DIR/lib
 #tmux source $gbn_include/gbn-colors.conf
 #tmux source $TMUX_ROOT/tmux.conf
-tmux display "Gruvbox-Next ready"
+tmux setenv HASRUN 1
+tmux display "Gruvbox-Next ready HASRUN: #{HASRUN}"
