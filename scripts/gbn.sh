ae7a2770eec992c2106c7ce651c5a714aa30aa5a
lucifer-morningstar <slobeck@gmail.com>
2025-09-03 07:23:33 -0700

    more gruvbox

--- scripts/gbn.sh
+++ scripts/gbn.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
-gbn_include=$CURRENT_DIR/lib
-#tmux source $gbn_include/gbn-colors.conf
+LIB=$CURRENT_DIR/lib
+#tmux source $LIB/gbn-colors.conf
 #tmux source $TMUX_ROOT/tmux.conf
-[ ! -d $CURRENT_DIR/data  ] && mkdir $CURRENT_DIR/data
-if [ -n $CURRENT_DIR/data/state ]; then
+DATA="$CURRENT_DIR/data"
+[ ! -d $DATA  ] && mkdir $CURRENT_DIR/data
+if [ -n $DATA ]; then
   # check the hashes for each file in the list and compare them 
   # against the ones listed in data/state. If missing or mismatched
   # then source the file, record timehe new hash for each as they load.
-  OHASH=
-  NHASH=
-  for file in $CURRENT_DIR/lib/*; do
-    #OHASH=$( grep -ei "$file" | awk '{print $2}' )
-    #NHASH=sha256sum $file 
-    tmux display -p "Checkin hashes for $file"
+  tmux display -p "Checkin hashes for $file"
+  for file in $LIB/*; do
+    in="$( grep -ei "$file" "$DATA" )"
+    if [[ -z "${in}"  ]];then
+      ohash="$( printf '%s %s\n',"$( sha256sum $file )","$file" )"
+      echo "$ohash" >> "$DATA"
+    else
+      ohash="$in"
+      nhash="$( printf '%s %s\n',"$( sha256sum $file )","$file" )"
+    fi
+    if [[ ! "${ohash}" = "${nhash}" ]]; then
+      sed -i "/${file}/s/^.*$/${nhash}/" "$DATA"
+      source "$file"
+      tmux refresh-client
+    fi
     done
 fi
 tmux setenv HASRUN 1
