cdfb7fefe426599d4312f5843d1ee16a20dcf501
lucifer-morningstar <slobeck@gmail.com>
2025-09-02 22:15:36 -0700

    stripped back everything

--- scripts/gbn.sh
+++ scripts/gbn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-gbn_include=$TMUX_PLUGIN_MANAGER_PATH/gruvbox-next/lib
-tmux source $gbn_include/gbn-colors.conf
-tmux source $TMUX_ROOT/tmux.conf
+gbn_include=$CURRENT_DIR/lib
+#tmux source $gbn_include/gbn-colors.conf
+#tmux source $TMUX_ROOT/tmux.conf
 tmux display "Gruvbox-Next ready"
