8c8b0ce20f8c60c6cae0b659072eaff16c962f7e
lucifer-morningstar <slobeck@gmail.com>
2025-09-05 09:13:49 -0700

    gbn.sh test

--- scripts/gbn.sh
+++ scripts/gbn.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-tmux display 'gbn.sh'
-tmux source "$LIB/gbn-colors.conf"
+tmux run-shell "bash $LIB/gbn-colors.conf"
 #tmux source $TMUX_ROOT/tmux.conf
 #tmux set-option -g @Red "#fb4934"
 #tmux setenv HASRUN 1
