03b5d5aa95dbcae1498026abb3ddc66bb7c606bb
lucifer-morningstar <slobeck@gmail.com>
2025-09-05 09:05:11 -0700

    more fixes

--- scripts/gbn.sh
+++ scripts/gbn.sh
@@ -2,6 +2,6 @@
 tmux display 'gbn.sh'
 #tmux source $LIB/gbn-colors.conf
 #tmux source $TMUX_ROOT/tmux.conf
-
+tmux set-option -g @Red "#fb4934"
 #tmux setenv HASRUN 1
 tmux display "Gruvbox-Next ready HASRUN: #{HASRUN}"
--- scripts/push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-COMMENT=$1
-git add -A
-git commit -a -m "${COMMENT:-unset}"
-git push -u origin main
