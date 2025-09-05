39d715e13054db8b29403990823c75c799f98b40
lucifer-morningstar <slobeck@gmail.com>
2025-09-05 10:30:56 -0700

    fixes

--- scripts/gbn.sh
+++ scripts/gbn.sh
@@ -5,10 +5,9 @@ local name=""
 while read -r line; do
   $name="@${line%%:*}";
   $color="\"${line##*:}\"";
+  printf "tmux set -g ${name} ${color}\n",$1,$2
   tmux set-option -g "${name}" "${color}"
 done
 tmux refresh-client
-#tmux source $TMUX_ROOT/tmux.conf
 
-#tmux setenv HASRUN 1
 tmux display "Gruvbox-Next ready HASRUN: #{HASRUN}"
