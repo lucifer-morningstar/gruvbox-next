1656a50f6eeb2a4c6c59dc1a2532be164122197e
lucifer-morningstar <slobeck@gmail.com>
2025-09-05 09:11:27 -0700

    gbn.sh test

--- scripts/gbn.sh
+++ scripts/gbn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 tmux display 'gbn.sh'
-#tmux source $LIB/gbn-colors.conf
+tmux source "$LIB/gbn-colors.conf"
 #tmux source $TMUX_ROOT/tmux.conf
-tmux set-option -g @Red "#fb4934"
+#tmux set-option -g @Red "#fb4934"
 #tmux setenv HASRUN 1
 tmux display "Gruvbox-Next ready HASRUN: #{HASRUN}"
